a4e134e2fd3c4aa1e6e53f406ff1bd99b2a2326f
melicarls <melissacarlson15@gmail.com>
2016-05-27 12:26:25 -0700

    added user foreign key to route model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527192439) do
+ActiveRecord::Schema.define(version: 20160527192554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,10 @@ ActiveRecord::Schema.define(version: 20160527192439) do
     t.boolean  "favorite"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "user_id"
   end
+
+  add_index "routes", ["user_id"], name: "index_routes_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -65,4 +68,5 @@ ActiveRecord::Schema.define(version: 20160527192439) do
   end
 
   add_foreign_key "activities", "users"
+  add_foreign_key "routes", "users"
 end
