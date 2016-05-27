a89d6b7efc85be32e2520afb7fe87f88ad43945c
melicarls <melissacarlson15@gmail.com>
2016-05-26 22:13:03 -0700

    created user model

---
+++ db/migrate/20160527051012_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :image_url
+      t.string :city
+      t.string :state
+      t.string :token
+      t.string :email
+      t.string :strava_id
+
+      t.timestamps null: false
+    end
+  end
+end
