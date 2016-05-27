bb645a5fb165c4a0f3bff0bf29b827145836ccb5
melicarls <melissacarlson15@gmail.com>
2016-05-26 22:07:09 -0700

    successfully accessed strava and created a session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
+    p auth_hash
     render :text => auth_hash.inspect
   end
 
   def failure
+    flash[:error] = "Your account could not be authenticated"
+    redirect_to :index
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_API_KEY'], scope: 'public'
+  provider :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_API_KEY']
 end
