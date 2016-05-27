d6248e995815e0ff3731ebf39500729ba5b6ca5f
melicarls <melissacarlson15@gmail.com>
2016-05-26 21:43:42 -0700

    configured use of .env file and create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/login', :to => 'sessions#new', :as => :login
+  get '/auth/:provider/callback', :to => 'sessions#create'
+  get '/auth/failure', :to => 'sessions#failure'
+
   root 'site#index'
 
   get '*path', to: 'site#index'
